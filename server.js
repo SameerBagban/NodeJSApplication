f2730595a4f9787fdf2e9a54af87129ee7952fbc
Siraj Chaudhary <mr.sirajchaudhary@rocketmail.com>
2018-03-21 04:41:20 +0000

    first commmit!

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var fs = require('fs');
-
 http.createServer(function(req, res){
     fs.readFile('index.html',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
